681b1029b9bdb742fafd72b3cfa666282415627c
Noah Watkins <noahwatkins@gmail.com>
2018-02-07 15:03:42 -0800

    db: add intention0 to committed intention index
    fixes: #62
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/db/db.cc
+++ src/db/db.cc
@@ -2,7 +2,17 @@
 #include <unistd.h>
 #include <sstream>
 #include <chrono>
+#include <iomanip>
 #include <spdlog/spdlog.h>
+
+static std::string prefix_string(const std::string& prefix,
+    const std::string& value)
+{
+  auto out = prefix;
+  out.push_back(0);
+  out.append(value);
+  return out;
+}
 
 namespace cruzdb {
 
@@ -48,13 +58,36 @@ int DB::Open(zlog::Log *log, bool create_if_empty, DB **db,
       assert(pos == 0);
     }
 
-    // write a corresponding after image. when/if we start adding some initial
-    // data to the database, then we'll need to make sure that this after image
-    // reflects that.
+    // build and write the corresponding afterimage.
+    //
+    // TODO: ideally we would _not_ write this afterimage, and would package any
+    // initial data up into the init intention, and let it be processed as
+    // normal. to do this we want to avoid special cases. one way to avoid these
+    // special cases might be to fill position 0, write the init intention at
+    // position 1, using a snapshot of position 0 so it is serial.
     {
       cruzdb_proto::AfterImage after_image;
+
+      // add intention 0 to the committed intention index
+      std::stringstream ci_key;
+      ci_key << std::setw(20) << std::setfill('0') << 0;
+
+      auto node = after_image.add_tree();
+      node->set_red(false);
+      node->set_key(prefix_string(PREFIX_COMMITTED_INTENTION, ci_key.str()));
+      node->set_val("");
+
+      auto left = node->mutable_left();
+      left->set_nil(true);
+      left->set_self(false);
+
+      auto right = node->mutable_right();
+      right->set_nil(true);
+      right->set_self(false);
+
       after_image.set_intention(0);
 
+      // serialize and append
       cruzdb_proto::LogEntry entry;
       entry.set_type(cruzdb_proto::LogEntry::AFTER_IMAGE);
       entry.set_allocated_after_image(&after_image);
